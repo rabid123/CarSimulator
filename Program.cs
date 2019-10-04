4d366f2680b29541e2f1bb3f8563d1b8e9253e16
Rabid <rabidm@hotmail.com>
2019-10-04 11:18:59 +0200

    Add abiliy to add car

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace CarSimulator
 {
@@ -6,7 +7,42 @@ namespace CarSimulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            bool shouldNotExit = true;
+
+            while (shouldNotExit)
+
+            {
+                WriteLine("1. Add car");
+                WriteLine("2. List cars");
+                WriteLine("3. simulate speed");
+                WriteLine("4. Exit");
+
+                ConsoleKeyInfo keyPressed = ReadKey(true);
+                Clear();
+
+                switch (keyPressed.Key)
+                {
+
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+
+                        break;
+
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+
+                        break;
+
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+                        shouldNotExit = false;
+                        break;
+
+                }
+                Clear();
+            }
+
         }
     }
 }
